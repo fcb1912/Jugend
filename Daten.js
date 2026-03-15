2686f68e0776c2b5c011771d2faacade953170e9
fcb1912 <jugend@fc-badenia-stilgen.de>
2026-03-15 22:41:27 +0100

    Add files via upload

---
+++ Daten.js
@@ -0,0 +1,68 @@
+const trainingsDaten = [
+  {
+    "team": "G3-JUGEND / MINI-BAMBINI (U5)",
+    "category": "bambini",
+    "trainer": "Lisa Schönig, Frank Huber",
+    "zeiten": ["Do.: 16:00–16:45 – Waldstadion"]
+  },
+  {
+    "team": "G2-JUGEND / BAMBINI (U6)",
+    "category": "bambini",
+    "trainer": "Kutsal Yolasar, Lara Leblebici",
+    "zeiten": ["Mo. & Mi.: 16:00–17:00 – Waldstadion"]
+  },
+  {
+    "team": "G1-JUGEND / BAMBINI (U7)",
+    "category": "bambini",
+    "trainer": "Natascha Motta, Sascha Motta, Alexander Brecht",
+    "zeiten": ["Mo. & Mi.: 16:00–17:00 – Waldstadion"]
+  },
+  {
+    "team": "F2-JUGEND (U8)",
+    "category": "grundlagen",
+    "trainer": "Sven Uhlenhuth, Peter Jeremias, Alexander Probst",
+    "zeiten": ["Mo. & Mi.: 16:45–18:00 – Waldstadion"]
+  },
+  {
+    "team": "F1-JUGEND (U9)",
+    "category": "grundlagen",
+    "trainer": "Jessica Ruck, Silas Koch, Monika Ruck, Heiko Ruck",
+    "zeiten": ["Mo. & Mi.: 16:45–18:00 – Waldstadion"]
+  },
+  {
+    "team": "E2-JUGEND (U10)",
+    "category": "grundlagen",
+    "trainer": "Christian Wüst, Claus Hammer, Uwe Schneider",
+    "zeiten": ["Mo. & Mi.: 16:00–17:30 – Waldstadion"]
+  },
+  {
+    "team": "E1-JUGEND (U11)",
+    "category": "grundlagen",
+    "trainer": "Pierre Örüm, David Ovat, Alexander Schabunja, Maxim Stiben, Denis Strelzow",
+    "zeiten": ["Di. & Do.: 17:15–18:45 – Waldstadion"]
+  },
+  {
+    "team": "D-JUGEND (U13)",
+    "category": "grundlagen",
+    "trainer": "Alexsandro Addis",
+    "zeiten": ["Di. & Do.: 17:15–18:45 – Waldstadion"]
+  },
+  {
+    "team": "C-JUGEND (U15)",
+    "category": "aufbau",
+    "trainer": "Benjamin Winkler",
+    "zeiten": ["Di. & Do.: 17:30–19:00 – Waldstadion"]
+  },
+  {
+    "team": "B-JUGEND (U17) - JSG RAUENBERG/ST. ILGEN",
+    "category": "aufbau",
+    "trainer": "Gabriel Phillipp, Cosmin Udovita (beide VfB Rauenberg)",
+    "zeiten": ["Mo.: 17:30–19:00 – Waldstadion", "Do.: 17:30–19:00 – Rauenberg"]
+  },
+  {
+    "team": "A-JUGEND (U20)",
+    "category": "aufbau",
+    "trainer": "Serdar Ay",
+    "zeiten": ["Di. & Do.: 18:00–19:30 – Waldstadion"]
+  }
+];
